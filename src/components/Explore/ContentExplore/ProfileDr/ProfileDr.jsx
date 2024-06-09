17d43e5e4b35eb3f88c39362648b4aa5d1359ee5
unknown <chamimeriem2302@gmail.com>
2024-06-09 12:10:42 +0100

    add-profile-features

--- src/components/Explore/ContentExplore/ProfileDr/ProfileDr.jsx
+++ src/components/Explore/ContentExplore/ProfileDr/ProfileDr.jsx
@@ -4,7 +4,10 @@ import { useLocation } from "react-router-dom";
 
 function ProfileDr({ profile }) {
   const location = useLocation();
-  const selectedProfile = location.state ? location.state.selectedProfile : null;
+  const selectedProfile = location.state
+    ? location.state.selectedProfile
+    : null;
+  const username = profile?.username || selectedProfile?.username;
 
   if (!profile && !selectedProfile) {
     return (
@@ -14,7 +17,20 @@ function ProfileDr({ profile }) {
     );
   }
 
-  const username = profile?.username || selectedProfile?.username;
+  const testSkills = [
+    {
+      id: 1,
+      title: "lorem lorem",
+    },
+    {
+      id: 2,
+      title: "lorem lorem",
+    },
+    {
+      id: 3,
+      title: "lorem lorem",
+    },
+  ];
 
   return (
     <div className="flex flex-col">
@@ -24,6 +40,31 @@ function ProfileDr({ profile }) {
           <h2 className="font-bold mt-20 text-2xl self-center px-6">
             {username}
           </h2>
+        </div>
+      </div>
+      <div className="py-14 px-14">
+        <div>
+          <h3 className="font-meduim text-3xl pt-10 pb-4">Pitch</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            impedit suscipit odit. Ea sed ex deleniti obcaecati aliquid. Vel
+            minus et labore adipisci nisi? Mollitia aliquid dolores officia
+            ratione est.
+          </p>
+        </div>
+
+        <div className=" flex flex-col">
+          <h3 className="font-meduim text-3xl pt-10 pb-4">Skills</h3>
+          <div className="flex gap-3">
+            {testSkills.map(({ id, title }) => (
+              <div
+                key={id}
+                className="border-2 rounded-xl bg-gray-100 px-4 py-2"
+              >
+                <p>{title}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
