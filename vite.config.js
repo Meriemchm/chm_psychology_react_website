46e3e63a1086303151437cc32e619b402adb3a8a
meriem meriem <chamimeriem2302@gmail.com>
2024-07-10 21:22:39 +0100

    test env

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_BASE_URL,
+        target: import.meta.env.VITE_API_BASE_URL ,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
