166ab1c5c660f869768ba2fea389152aaa385bd5
meriem meriem <chamimeriem2302@gmail.com>
2024-07-12 19:40:40 +0100

    comment firebase code

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+// import { getStorage } from "firebase/storage";
 
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_KEY,
-  authDomain: "rameem-2024.firebaseapp.com",
-  projectId: "rameem-2024",
-  storageBucket: "rameem-2024.appspot.com",
-  messagingSenderId: "380008552996",
-  appId: "1:380008552996:web:2ec9f4941cc74e4dcc56b1",
-};
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_KEY,
+//   authDomain: "rameem-2024.firebaseapp.com",
+//   projectId: "rameem-2024",
+//   storageBucket: "rameem-2024.appspot.com",
+//   messagingSenderId: "380008552996",
+//   appId: "1:380008552996:web:2ec9f4941cc74e4dcc56b1",
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth()
-export const storage = getStorage(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+// export const auth = getAuth()
+// export const storage = getStorage(app);
